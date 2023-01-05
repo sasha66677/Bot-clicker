f8491f131cd4e3b1b6d29260c2ce2c94e50a40ac
sasha66677 <123123salo21082018@gmail.com>
2023-01-05 13:21:00 +0300

    Добавьте файлы проекта.

---
+++ Bot.cpp
@@ -0,0 +1,15 @@
+﻿#include <windows.h>
+
+#define X (DWORD)50
+#define Y (DWORD)50
+
+int main(int argc, char** argv) {
+	Sleep(10000);
+	for (int i = 0; i <= 100; ++i) {
+		mouse_event(MOUSEEVENTF_RIGHTDOWN, X, Y, 0, 0);
+		Sleep(200);
+		mouse_event(MOUSEEVENTF_RIGHTUP, X, Y, 0, 0);
+		Sleep(200);
+	}
+	return (EXIT_SUCCESS);
+}
